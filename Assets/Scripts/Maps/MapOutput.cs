9267068326e092967898c7f7326b52df4fb5c14c
STD\s20227036 <20227036-takemuraayato@hcs.ac.jp>
2024-03-13 17:21:33 +0900

    スクリプト削減

--- Assets/Scripts/Maps/MapOutput.cs
+++ Assets/Scripts/Maps/MapOutput.cs
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------  
-// MapOutput.cs  
-// マップ配列にTileMapの状態を出力する
-// 作成日:  3/11
+// NewMapOut.cs  
+//   
+// 作成日:  3/12
 // 作成者:  竹村綾人
 // ---------------------------------------------------------  
 using UnityEngine;
@@ -13,29 +13,28 @@ public class MapOutput : MonoBehaviour
 {
 
     #region 変数  
-
-    [SerializeField, Tooltip("マップ配列を保持しているスクリプト")]
-    private MapData _mapData = default;
-
     [SerializeField, Tooltip("マップ配列に格納したいタイルマップ")]
     private Tilemap _tileMap = default;
     [SerializeField, Tooltip("使用するタイルを格納")]
     private List<Tile> _tileType = new List<Tile>();
-
     [Tooltip("タイルマップの横サイズ")]
     private int _horizontal = default;
     [Tooltip("タイルマップの縦サイズ")]
     private int _vartical = default;
     [Tooltip("タイルマップのX座標で一番小さい値")]
-    private int _horizontalMin = default;
-    [Tooltip("タイルマップのY座標で一番大きい値")]
+    private int _horizontalMin = default;    
+    [Tooltip("タイルマップのX座標で一番大きい値")]
     private int _varticalMax = default;
+    [Tooltip("タイルマップのY座標で一番小さい値")]
+    private int _varticalMin = default;
     [Tooltip("配列が0から始まるため、引く数字")]
     private const int CONST_MINAS_ONE = -1;
-
-    [Tooltip("曲がり角の座標")]
-    private List<Vector2Int> _curvePos = new List<Vector2Int>();
-
+    [SerializeField,Tooltip("曲がり角の座標")]
+    private Vector3[] _curvePos = default;
+    [Tooltip("ルート探索スクリプト")]
+    private MapRouteSearch _mapRouteSearch = default;
+    [Tooltip("曲がり角の数")]
+    private int _curveCount = default;
     #endregion
 
     #region プロパティ  
@@ -43,46 +42,37 @@ public class MapOutput : MonoBehaviour
     #endregion
 
     #region メソッド  
-
-    /// <summary>  
-    /// 初期化処理  
-    /// </summary>  
-    void Awake()
-     {
-        Output();
-     }
   
      /// <summary>  
      /// 更新前処理  
      /// </summary>  
      void Start ()
      {
-  
+        Output();
      }
-  
-     /// <summary>  
-     /// 更新処理  
-     /// </summary>  
-     void Update ()
-     {
-     }
-
 
     /// <summary>
     /// ボタンが押されたらタイルマップを配列に落としこむ
     /// </summary>
     public void Output() {
+        //左下を原点にする
         BoundsInt bounds = _tileMap.cellBounds;
         //タイルマップの一番左と一番下の座標を格納
         _horizontalMin = bounds.min.x;
-        _varticalMax = bounds.max.y;
+        _varticalMin = bounds.min.y;
+        _varticalMax = bounds.max.y + CONST_MINAS_ONE;
+        Debug.LogError("a:" + _varticalMin + "" + _horizontalMin);
+        Debug.LogError("b:" + bounds.max.x + "" + bounds.max.y);
+        //0からどのくらい離れているか
+        int xDistance = -_horizontalMin;
+        int yDistance = -_varticalMin;
         //タイルマップのサイズを調べる
-        _horizontal = bounds.max.x - _horizontalMin;
-        _vartical = _varticalMax - bounds.min.y;
+        _horizontal = xDistance + bounds.max.x;
+        _vartical = yDistance + bounds.max.y;
         //タイルマップの情報のサイズに配列を変更
-        _mapData.ArraySizeChange(_horizontal, _vartical);
+        MapData.Instance.ArraySizeChange(_vartical, _horizontal);
 
-        //配列を変更
+        //タイルマップのすべてのタイルの枚数繰り返す
         foreach (Vector3Int pos in _tileMap.cellBounds.allPositionsWithin) {
             // タイルが無かったら
             if (!_tileMap.HasTile(pos)) {
@@ -95,12 +85,11 @@ public class MapOutput : MonoBehaviour
                 // スプライトが一致しているか判定
                 if (_tileMap.GetTile(pos) == tile) {
                     // 特定のスプライトと一致している場合は配列のそのタイルに対応した数字を格納
-                    _mapData.MapDataArray[_varticalMax - pos.y + CONST_MINAS_ONE, pos.x - _horizontalMin] = index;
+                    MapData.Instance.MapDataArray[bounds.max.y - 1 - pos.y, pos.x + xDistance] = index;
                     break;
                 }
                 index++;
             }
-
         }
 
         // 配列を出力するテスト
@@ -108,25 +97,23 @@ public class MapOutput : MonoBehaviour
         for (int y = 0; y < _vartical; y++) {
             string outPutString = "";
             for (int x = 0; x < _horizontal; x++) {
-                outPutString += _mapData.MapDataArray[y, x];
+                outPutString += MapData.Instance.MapDataArray[y, x];
             }
             print(outPutString);
         }
         print("Field------------------------------------------");
+        print("A" + MapData.Instance.MapDataArray.GetLength(0) + "B" + MapData.Instance.MapDataArray.GetLength(1));
 
-        //配列を使って経路探索
-
-        //ポジション配列が帰ってくるため、それをタイルマップの座標に変換
-        //foreach (Vector3Int pos in _tileMap.cellBounds.allPositionsWithin) {
-        //    // タイルが無かったら
-        //    if (!_tileMap.HasTile(pos)) {
-        //        //処理を中断
-        //        return;
-        //    }
-        //    //配列に追加
-        //    //_curvePos.Add(new Vector2Int(0,0));
-        //    //_mapData.MapDataArray[_varticalMax - pos.y + CONST_MINAS_ONE, pos.x - _horizontalMin] =
-        //}
+        //ルートを探索する(配列内)
+        _mapRouteSearch = new MapRouteSearch(MapData.Instance.MapDataArray.GetLength(0), MapData.Instance.MapDataArray.GetLength(1), MapData.Instance);
+        //曲がり角の数を取得する
+        _curveCount = _mapRouteSearch.CurvePosition.Count;
+        //曲がり角の数と同じ要素数の配列を生成
+        _curvePos = new Vector3[_curveCount];
+        //生成した配列にタイルマップの座標をワールド座標で格納
+        for (int index = 0; index < _curveCount; index++) {
+            _curvePos[index] = _tileMap.GetCellCenterWorld(new Vector3Int(_mapRouteSearch.CurvePosition[index][1] + _horizontalMin, -_mapRouteSearch.CurvePosition[index][0] + _varticalMax));
+        }
     }
     #endregion
 }
--- Assets/Scripts/Maps/MapRouteSearch.cs
+++ Assets/Scripts/Maps/MapRouteSearch.cs
@@ -1,52 +1,87 @@
 // ---------------------------------------------------------  
-// MapRouteSearch.cs  
-// 敵の経路探索スクリプト
-// 作成日:  3/11
-// 作成者:  竹村綾人
+// NewMapRoute.cs  
+//   
+// 作成日:  
+// 作成者:  
 // ---------------------------------------------------------  
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Tilemaps;
+using System;
 
-public class MapRouteSearch : MonoBehaviour {
+public class MapRouteSearch
+{
 
     #region 変数  
 
-    [Tooltip("一個前に探索していた配列座標")]
-    private Vector2Int _beforePosition = default;
-    [Tooltip("曲がり角の配列座標格納リスト")]
-    private List<int[]> _curvePosition = new List<int[]>();
-    [SerializeField, Tooltip("マップ配列スクリプト")]
+    //一個前に探索していた配列座標
+    private int[] _beforePosition = new int[2];
+    //マップ配列スクリプト
     private MapData _mapData = default;
-    [SerializeField, Tooltip("タイルマップオブジェクト")]
-    private Tilemap _tilemap = default;
-    [Tooltip("探索開始地点")]
-    private Vector2Int _searchedPos = default;
-    [Tooltip("配列で道を示す数字")]
-    private int _roadInt = 1;
-    [SerializeField, Tooltip("道のタイル数")]
+    //探索開始地点
+    private int[] _searchedPos = new int[2];
+    //配列で道を示す数字
+    private const int CONST_ROAD_NUMBER = 1;
+    //配列でスタートを示す数字
+    private const int CONST_START_NUMBER = 2;
+    //配列でゴールを示す数字
+    private const int CONST_GOAL_NUMBER = 3;
+    //道のタイル数
     private int _tileCount = 0;
-    [SerializeField, Tooltip("曲がり角数")]
+    //曲がり角数
     private int _curveCount = 0;
-    [Tooltip("曲がり角の座標格納配列")]
-    private List<Vector2Int> _curvePoints = new List<Vector2Int>();
-    [Tooltip("曲がり角確認用(0:上 1:左 2:下 3:右)")]
+    //曲がり角の座標格納配列
+    private List<int[]> _curvePosition = new List<int[]>();
+    //曲がり角確認用(0:上 1:左 2:下 3:右)
     private int _index = default;
-    [Tooltip("マップ配列のy軸の要素数")]
+    //マップ配列のy軸の要素数
     private int _yLength = default;
-    [Tooltip("マップ配列のx軸の要素数")]
+    //マップ配列のx軸の要素数
     private int _xLength = default;
+    //配列のY座標の配列番号
+    private const int CONST_Y_NUMBER = 0;
+    //配列のX座標の配列番号
+    private const int CONST_X_NUMBER = 1;
 
+    private int[] _searchPos = new int[2];
+
+    private int _error = 0;
 
     //進む方向
-    private Vector2Int[] _fourDorection =
+    private int[,] _fourDirection =
     {
-            new Vector2Int(-1, 0) ,       //上
-            new Vector2Int(0, 1),        //右
-            new Vector2Int(1, 0),       //下
-            new Vector2Int(0, -1)        //左
+            { -1, 0 },       //上
+            { 0, 1},        //右
+            { 1, 0},        //下
+            { 0, -1}        //左
     };
+
+    public List<int[]> CurvePosition {
+        get => _curvePosition;
+        set => _curvePosition = value;
+    }
+
+
+
+
+
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public MapRouteSearch(int yLength,int xLength, MapData mapData) {
+        //マップ配列のy軸の要素数
+        _yLength = yLength;
+        //マップ配列のx軸の要素数
+        _xLength = xLength;
+        //マップ配列スクリプト
+        _mapData = mapData;
+        //ルート探索開始
+        StartSearch1();
+    }
+
+
+
+
 
     #endregion
 
@@ -56,121 +91,90 @@ public class MapRouteSearch : MonoBehaviour {
 
     #region メソッド  
 
-    /// <summary>  
-    /// 初期化処理  
-    /// </summary>  
-    void Awake() {
+    /// <summary>
+    /// 探索開始位置を探す
+    /// </summary>
+    public void StartSearch1() {
+        int indexX = 0;
+        int indexY = 0;
+
+        //探索開始位置を探す
+        foreach (int startPos in _mapData.MapDataArray) {
+            //探索開始位置だったら
+            if (startPos == CONST_START_NUMBER) {
+                //探索位置の座標を格納
+                _searchedPos[CONST_Y_NUMBER] = indexY;
+                _searchedPos[CONST_X_NUMBER] = indexX;
+                break;
+            }
+            indexX++;
+            //もしxが
+            if (indexX >= _xLength) {
+                indexY++;
+                indexX = 0;
+            }
+        }
+        //ルート探索開始
+        SearchRoute1(0);
     }
 
-    /// <summary>  
-    /// 更新前処理  
-    /// </summary>  
-    void Start() {
-        //タイルマップのワールド座標を取得
-        //_tilemap.GetCellCenterWorld(new Vector3Int(1, 2, 0));
-
-        RouteSearch(new Vector2Int(0, 13));
-    }
-
-    /// <summary>  
-    /// 更新処理  
-    /// </summary>  
-    void Update() {
-    }
 
     /// <summary>
-    /// ルート探索
+    /// ルート探索をする
     /// </summary>
-    /// <param name="startPos">開始地点</param>
-    private void RouteSearch(Vector2Int startPos) {
-        //指定された位置から探索開始する
-        _searchedPos = startPos;
-        //インデックス初期化
-        _index = 0;
+    private void SearchRoute1(int tileCount) {
+        _tileCount = tileCount;
         //今いるマスの周り４方向を調べる(上右下左の順で探索)
-        foreach (Vector2Int vector2Int in _fourDorection) {
-            //元いた地点を調べてるなら
-            if (new Vector2Int(_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y) == _beforePosition) {
+        for (int y = 0; y < 4; y++) {
+            //調べている地点を格納
+            _searchPos[CONST_Y_NUMBER] =  _searchedPos[CONST_Y_NUMBER] + _fourDirection[y, CONST_Y_NUMBER];
+            _searchPos[CONST_X_NUMBER] =  _searchedPos[CONST_X_NUMBER] + _fourDirection[y, CONST_X_NUMBER];
+
+            //元いた地点を調べているなら
+            if (_searchPos[CONST_Y_NUMBER] == _beforePosition[CONST_Y_NUMBER] && _searchPos[CONST_X_NUMBER] == _beforePosition[CONST_X_NUMBER]) {
+                //次の方向を調べる
                 continue;
             }
-            //もし調べてる地点が配列範囲外なら
-            if (_searchedPos.x + vector2Int.x < 0 || _searchedPos.x + vector2Int.x > 9 || _searchedPos.y + vector2Int.y < 0 || _searchedPos.y + vector2Int.y > 17) {
+            //配列範囲外を調べているなら
+            if (_searchPos[CONST_Y_NUMBER] < 0 || _searchPos[CONST_Y_NUMBER] > _yLength || _searchPos[CONST_X_NUMBER] < 0 || _searchPos[CONST_X_NUMBER] > _xLength) {
+                //次の方向を調べる
                 continue;
             }
-            //もし道があるなら
-            if (_mapData.MapDataArray[_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y] == _roadInt) {
+            //道を調べているなら
+            if (_mapData.MapDataArray[_searchPos[CONST_Y_NUMBER], _searchPos[CONST_X_NUMBER]] == CONST_ROAD_NUMBER) {
                 //タイル数をカウントする
                 _tileCount++;
                 //曲がり角か
-                if (vector2Int != _searchedPos - _beforePosition) {
-                    //曲がり角の座標を格納
+                if ((_fourDirection[y,CONST_Y_NUMBER] != _searchedPos[CONST_Y_NUMBER] - _beforePosition[CONST_Y_NUMBER])&& (_fourDirection[y, CONST_X_NUMBER] != _searchedPos[CONST_X_NUMBER] - _beforePosition[CONST_X_NUMBER])) {
+                    //曲がり角数をカウントする
                     _curveCount++;
-                    //_curvePoints.Add()
-                    //    _tilemap.GetCellCenterWorld(new Vector3Int(1, 2, 0));
-                    //_varticalMax - pos.y + CONST_MINAS_ONE, pos.x - _horizontalMin
+                    //曲がり角の座標を格納
+                    CurvePosition.Add(new int[] { _searchedPos[CONST_Y_NUMBER], _searchedPos[CONST_X_NUMBER] });
                 }
 
                 //元居た地点を格納
-                _beforePosition = _searchedPos;
-                //次のタイルの周りを探索
-                RouteSearch(new Vector2Int(_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y));
-            }
-            //もし曲がり角なら配列に格納
-            //インクリメント
-            _index++;
-        }
+                _beforePosition[CONST_Y_NUMBER] = _searchedPos[CONST_Y_NUMBER];
+                _beforePosition[CONST_X_NUMBER] = _searchedPos[CONST_X_NUMBER];
+                //探索するタイルの座標を変更する
+                _searchedPos[CONST_Y_NUMBER] = _searchPos[CONST_Y_NUMBER];
+                _searchedPos[CONST_X_NUMBER] = _searchPos[CONST_X_NUMBER];
 
-
-
-        //曲がり角を取得し、配列に格納
-        //その間を敵のスピードで進む
-        //スタート地点を設定
-
-
-    }
-
-
-    private void RouteSearchSecond(Vector2Int startPos) {
-        //指定された位置から探索開始する
-        _searchedPos = startPos;
-        //
-
-
-        //今いるマスの周り４方向を調べる(上右下左の順で探索)
-        foreach (Vector2Int vector2Int in _fourDorection) {
-            //元いた地点を調べてるなら
-            if (new Vector2Int(_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y) == _beforePosition) {
-                Debug.Log("ONAJI");
-                continue;
-            }
-            //もし調べてる地点が配列範囲外なら
-            if (_searchedPos.x + vector2Int.x < 0 || _searchedPos.x + vector2Int.x > _mapData.MapDataArray.GetLength(0) || _searchedPos.y + vector2Int.y < 0 || _searchedPos.y + vector2Int.y > 17) {
-                Debug.Log("CONT");
-                continue;
-            }
-            //もし道があるなら
-            if (_mapData.MapDataArray[_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y] == _roadInt) {
-                Debug.Log("ROAD");
-                //タイル数をカウントする
-                _tileCount++;
-                Debug.LogError(vector2Int);
-                Debug.LogError(_searchedPos - _beforePosition);
-                //曲がり角か
-                if (vector2Int != _searchedPos - _beforePosition) {
-                    //曲がり角の座標を格納
-                    _curveCount++;
-                    //_curvePoints.Add()
-                    //    _tilemap.GetCellCenterWorld(new Vector3Int(1, 2, 0));
-                    //_varticalMax - pos.y + CONST_MINAS_ONE, pos.x - _horizontalMin
+                //エラー対策用
+                if (_error >= 200) {
+                    return;
                 }
-
-                //元居た地点を格納
-                _beforePosition = _searchedPos;
+                _error++;
                 //次のタイルの周りを探索
-                RouteSearch(new Vector2Int(_searchedPos.x + vector2Int.x, _searchedPos.y + vector2Int.y));
+                SearchRoute1(_tileCount);
+                return;
             }
-            //もし曲がり角なら配列に格納
-            print("NOT");
+            //もしゴールなら
+            else if (_mapData.MapDataArray[_searchPos[CONST_Y_NUMBER], _searchPos[CONST_X_NUMBER]] == CONST_GOAL_NUMBER) {
+                //ゴールのポジションを格納
+                CurvePosition.Add(new int[] { _searchPos[CONST_Y_NUMBER], _searchPos[CONST_X_NUMBER] });
+                return;
+            }
+
         }
     }
     #endregion
